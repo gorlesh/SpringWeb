0a7c57eb7402ae3c5003ba63f7aeb267336031cc
Shweta Gorle <shwetagorle@shwetas-air.lan>
2024-04-30 13:31:12 -0400

    commit

---
+++ Springbootweb/src/main/java/org/example/springbootweb/HomeController.java
@@ -0,0 +1,37 @@
+package org.example.springbootweb;
+
+
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home (){
+
+        return "index";
+    }
+
+    @RequestMapping("/add")
+    public ModelAndView add( int num1, int num2, ModelAndView mv){
+
+        int result=num1+num2;
+        mv.addObject("result",result);
+        mv.setViewName("result");
+        return mv;
+
+
+    }
+
+    @RequestMapping("addAlien")
+    public String addAlien(Alien alien) {
+        //public String addAlien(@ModelAttribute("alien1") Alien alien) {
+
+        return "result";
+    }
+}
---
+++ Springbootweb/src/main/webapp/views/result.jsp
@@ -0,0 +1,14 @@
+
+<%@page language="java" %>
+
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="../style.css">
+</head>
+</body>
+<h2>Telusko Calculator</h2>
+
+<h2> Result is : ${alien} </h2>
+
+</body>
+</html>
